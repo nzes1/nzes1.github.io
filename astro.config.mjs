e206bb9ac5db9452e421bc38f02ffbe8495c74a8
Nzesi <40242270+nzes1@users.noreply.github.com>
2026-02-24 08:44:07 +0300

    update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
     site: 'https://nzes1.github.io',
-    base: '/portfolio/',
 	integrations: [tailwind()],
     legacyCollections: true,
 });
